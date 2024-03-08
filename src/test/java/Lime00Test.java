d93913151a9bbd427524aa8e6c67c87b156d8451
Alex Ov <alexov@swisscows.email>
2024-03-08 20:52:27 +0200

    Add a test on opening chat window

---
+++ src/test/java/Lime00Test.java
@@ -0,0 +1,28 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class Lime00Test {
+
+    @BeforeEach
+    void setUp() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1920x1080";
+
+        open("https://lime-shop.com/ru_ru");
+        sleep(1500);
+        $("button[data-ok]").click();
+    }
+
+    @Test
+    void firstTest() {
+
+        $(".l-jivo-label").click();
+        $("textarea").shouldBe(Condition.visible)
+                .shouldHave(Condition.attribute("placeholder", "Введите сообщение"));
+
+    }
+}
